dce20e2da8c2ad5bdebc4e0952bfb5a897fef720
imuntil <imuntil@qq.com>
2018-04-28 10:24:47 +0800

    template -win

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-@Component
-export default class App extends Vue {
-  name: 'App'
-}
+  import { Vue, Component } from 'vue-property-decorator'
+  @Component
+  export default class App extends Vue {
+    name: 'App'
+  }
 </script>
 
 <style>
-#app {
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    height: inherit;
+    background-color: transparent;
+  }
 </style>
